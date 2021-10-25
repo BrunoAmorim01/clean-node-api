8cf3d0d78dffa65daf439ea58c2d9b5785bdcf81
Bruno Amorim <brunoamorim14@gmail.com>
2021-10-24 23:07:01 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,12 @@
-import { AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
-import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbAuthentication } from './db-authentication'
+import {
+  AccountModel,
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository,
+  TokenGenerator,
+  HashComparer,
+  AuthenticationModel
+} from './db-athentication-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,11 +1,11 @@
 import {
-  Authentication,
-  AuthenticationModel
-} from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository,
+  TokenGenerator,
+  HashComparer,
+  AuthenticationModel,
+  Authentication
+} from './db-athentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
