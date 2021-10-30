8c702887f2de847855af82c8cb3b6081dc7c4b3c
Bruno Amorim <brunoamorim14@gmail.com>
2021-10-29 22:55:47 -0300

    refactor: remove signuproute with loginroutes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 import app from '../config/app'
 
-describe('Signup routes', () => {
+describe('Login routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -13,9 +13,18 @@ describe('Signup routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should return an account on success', async () => {
-    await request(app).post('/api/signup')
-      .send({ name: 'Bruno', email: 'bruno@gmail.com', password: 'password', passwordConfirmation: 'password' })
-      .expect(200)
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Bruno',
+          email: 'bruno@gmail.com',
+          password: 'password',
+          passwordConfirmation: 'password'
+        })
+        .expect(200)
+    })
   })
 })
