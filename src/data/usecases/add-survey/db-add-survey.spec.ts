2e52d9ca3428d8bc38e351dbaf4635805f00968e
Bruno Amorim <brunoamorim14@gmail.com>
2021-11-06 21:41:35 -0300

    test: ensure DbAddSurvey trows if AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -33,4 +33,15 @@ describe('DbAddSurvey UseCase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+
+  test('Should throw Hasher if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest
+      .spyOn(addSurveyRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const promise = sut.add(makeSurveyData())
+    await expect(promise).rejects.toThrow()
+  })
 })
