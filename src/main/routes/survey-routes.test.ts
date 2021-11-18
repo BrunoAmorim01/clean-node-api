cc54888113cd02a1bd7c15dd09fc4e34ef8d6cac
Bruno Amorim <brunoamorim14@gmail.com>
2021-11-17 22:33:59 -0300

    test: ensure load surveys route returns 200 if a valid is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -67,5 +67,40 @@ describe('Survey routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('Should return 200 on Load Surveys with valid token', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'Bruno',
+        email: 'bruno@gmail.com',
+        password: '123'
+      })
+      const id = account.insertedId
+
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [{ image: 'any_image', answer: 'any_answer' }],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [{ image: 'other_image', answer: 'other_answer' }],
+          date: new Date()
+        }
+      ])
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
