845ee5b2c566ebb49ad9c494bf99d8a3c27e56cb
Bruno Amorim <brunoamorim14@gmail.com>
2021-12-27 18:59:11 -0300

    feat: add Swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import setUpMiddlewares from './middlewares'
 import setUpRoutes from './routes'
+import setUpSwagger from './config-swagger'
 import express from 'express'
 
 const app = express()
+setUpSwagger(app)
 setUpMiddlewares(app)
 setUpRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean-Node_Api',
+    description: 'Api do curso do mango para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+
+}
